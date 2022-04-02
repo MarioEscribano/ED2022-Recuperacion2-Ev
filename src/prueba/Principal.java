be8f3e9a1b5aa73197b4c9e535f717f6716da777
Mariokiller <mescribanor2320@gmail.com>
2022-04-02 18:39:44 +0200

    commit prueba

--- src/prueba/Principal.java
+++ src/prueba/Principal.java
@@ -3,7 +3,9 @@ package prueba;
 public class Principal {
 	
 	public static void main(String[] args) {
-		System.out.println("Este es un proyecto de git");
+		System.out.println("Este es un proyecto de git para github"
+				+ "parte 2");
+		System.out.println("Buenas tardes.");
 	}
 	
 }
